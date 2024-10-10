3f059676d3cf838c4c9b5339f8a4a9b55fa73831
Uítalo Souza <uitalo.urss@gmail.com>
2024-10-10 10:08:16 -0300

    fix error in swagger-output to show right options in document status

--- backend/src/modules/documents/domain/schemas/documentSchema.ts
+++ backend/src/modules/documents/domain/schemas/documentSchema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const documentSchema = z.object({
     name: z.string({required_error: "Campo nome é obrigatório."}).trim().min(3, "Nome precisa ter mais de 2 caracteres.").max(50, "Nome precisa ser menor que 50 caracteres."),
-    status: z.enum(["Não assinado", "Parcialmente assinado", "Assinado"], {required_error: "Campo status é obrigatório", message: "Favor usar as opções corretas."})
+    status: z.enum(["Não assinado", "Parcialmente assinado", "Assinado"], {required_error: "Campo status é obrigatório", message: "Favor usar as opções corretas.z"})
 })
 
 export const partialDocumentSchema = documentSchema.partial();
